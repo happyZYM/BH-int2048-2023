c7b55927a7e1d504de2c8b9bfa464813d2088ca6
ZhuangYumin <zhuangyumin@sjtu.edu.cn>
2023-10-24 17:28:57 +0800

    feat set up build system

---
+++ src/int2048.cpp
@@ -0,0 +1,136 @@
+#include "int2048.h"
+
+namespace sjtu {
+    // 构造函数
+    int2048::int2048() {
+        // 实现构造函数逻辑
+    }
+
+    int2048::int2048(long long) {
+        // 实现构造函数逻辑
+    }
+
+    int2048::int2048(const std::string &) {
+        // 实现构造函数逻辑
+    }
+
+    int2048::int2048(const int2048 &) {
+        // 实现构造函数逻辑
+    }
+
+    // 读入一个大整数
+    void int2048::read(const std::string &) {
+        // 实现读取逻辑
+    }
+
+    // 输出储存的大整数，无需换行
+    void int2048::print() {
+        // 实现输出逻辑
+    }
+
+    // 加上一个大整数
+    int2048 &int2048::add(const int2048 &) {
+        // 实现加法逻辑
+    }
+
+    // 返回两个大整数之和
+    int2048 add(int2048, const int2048 &) {
+        // 实现加法逻辑
+    }
+
+    // 减去一个大整数
+    int2048 &int2048::minus(const int2048 &) {
+        // 实现减法逻辑
+    }
+
+    // 返回两个大整数之差
+    int2048 minus(int2048, const int2048 &) {
+        // 实现减法逻辑
+    }
+
+    // 运算符重载
+
+    int2048 int2048::operator+() const {
+        // 实现一元加法逻辑
+    }
+
+    int2048 int2048::operator-() const {
+        // 实现一元减法逻辑
+    }
+
+    int2048 &int2048::operator=(const int2048 &) {
+        // 实现赋值运算符逻辑
+    }
+
+    int2048 &int2048::operator+=(const int2048 &) {
+        // 实现复合加法逻辑
+    }
+
+    int2048 operator+(int2048, const int2048 &) {
+        // 实现加法逻辑
+    }
+
+    int2048 &int2048::operator-=(const int2048 &) {
+        // 实现复合减法逻辑
+    }
+
+    int2048 operator-(int2048, const int2048 &) {
+        // 实现减法逻辑
+    }
+
+    int2048 &int2048::operator*=(const int2048 &) {
+        // 实现复合乘法逻辑
+    }
+
+    int2048 operator*(int2048, const int2048 &) {
+        // 实现乘法逻辑
+    }
+
+    int2048 &int2048::operator/=(const int2048 &) {
+        // 实现复合除法逻辑
+    }
+
+    int2048 operator/(int2048, const int2048 &) {
+        // 实现除法逻辑
+    }
+
+    int2048 &int2048::operator%=(const int2048 &) {
+        // 实现复合取模逻辑
+    }
+
+    int2048 operator%(int2048, const int2048 &) {
+        // 实现取模逻辑
+    }
+
+    std::istream &operator>>(std::istream &, int2048 &) {
+        // 实现输入运算符逻辑
+    }
+
+    std::ostream &operator<<(std::ostream &, const int2048 &) {
+        // 实现输出运算符逻辑
+    }
+
+    bool operator==(const int2048 &, const int2048 &) {
+        // 实现等于运算符逻辑
+    }
+
+    bool operator!=(const int2048 &, const int2048 &) {
+        // 实现不等于运算符逻辑
+    }
+
+    bool operator<(const int2048 &, const int2048 &) {
+        // 实现小于运算符逻辑
+    }
+
+    bool operator>(const int2048 &, const int2048 &) {
+        // 实现大于运算符逻辑
+    }
+
+    bool operator<=(const int2048 &, const int2048 &) {
+        // 实现小于等于运算符逻辑
+    }
+
+    bool operator>=(const int2048 &, const int2048 &) {
+        // 实现大于等于运算符逻辑
+    }
+}
